bd8e7156759808eb9f571e5e0fe5d81d08d7b07a
peterbugala <peterbugala@localhost>
2020-10-07 14:01:31 -0700

    Testing

---
+++ CSE 360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,29 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	private int total;
+
+
+	public AddingMachine () 
+	{
+		total = 0; // not needed - include for clarity
+	}
+	
+	public int getTotal () {			// Get total
+		return 0;
+	}
+	
+	public void add (int value) {		// Add method
+	}
+	
+	public void subtract (int value) { 	// Subtract method
+	}
+	
+	public String toString () {			// toString
+		return "";
+	}
+	
+	public void clear() {				// Clear
+	}
+}
